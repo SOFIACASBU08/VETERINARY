ad0fede0803d105f5a6351a832930ac5e11d1670
SOFIA CASTRILLON BUILES <sofiacastrillon287773@correo.itm.edu.co>
2023-09-28 21:48:07 -0500

    Visualización de ownersen la WEB desde el API

---
+++ veterinary.API/Controllers/OwnersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using veterinary.API.DATA;
+using veterinary.shared.Entities;
+
+namespace veterinary.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/owners")]
+    public class OwnersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+
+
+        public OwnersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //el get es como una busqueda
+        //GET POR LISTA
+        // select * from owners
+        [HttpGet]
+
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Owners.ToListAsync());
+        }
+
+        //GET POR PARAMETRO
+        [HttpGet("{id:int}")]
+
+        public async Task<ActionResult> Get(int id)
+        {
+           var owner = await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (owner == null) {
+                return NotFound();
+            }
+
+            return Ok(owner);
+        }
+
+        //el post es como un insert, inserta un registro.
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+            _context.Add(owner);
+            await _context.SaveChangesAsync();  
+           
+            return Ok(owner);
+        }
+    }
+}
---
+++ veterinary.web/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace veterinary.web.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> post<T, TResponse>(string url, T model);
+
+
+
+    }
+}
